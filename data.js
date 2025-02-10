bd99cce339e0f439edd0c302de7960b770f1c656
pyunaa <feunnahi@gmail.com>
2025-02-10 21:17:48 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+function get_images(){
+    return ['11.jpg', '1.jpg.jpg', '6.jpg', '3.jpg.jpg', '4.jpg.jpg', '5.jpg.jpg', '10.jpg.', '9.jpg', '8.jpg', '7.jpg', '2.jpg.jpg'];
+}
+function get_message(){
+    return [   'Dont away me na ha hahahha','I am grateful and lucky to have you' ,'I also want you to know that..', 'Just go here and youll be like "oh I have my girl pala na makulit"', 'So that if you feel that you are alone', 'I placed a bunch of pics of us', 'And that I appreciate you a lot ', "This is to remind you that I love so much, always", "Hi babyyyyy",'Sorry agad ta ney medyo magulo', 'PLAY THE MUSIC FIRSTTTTTTT'];
+}
